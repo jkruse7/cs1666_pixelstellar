c265af9423dc245542301070decfae25c152f159
Jordan Shopp <jordan.shopp123@gmail.com>
2024-10-21 20:30:39 -0400

    Revert "Procedural generation update"

--- src/particle/components.rs
+++ src/particle/components.rs
@@ -28,8 +28,6 @@ pub enum ParticleData {
     BedRock,
     Water,
     Sand,
-    Dirt,
-    Stone,
     // TODO: add more particle types
 }
 
@@ -62,7 +60,7 @@ impl BedRockParticle {
     pub fn new(x: i32, y: i32) -> Self {
         // random color range
         let mut rng = rand::thread_rng();
-        let black = rng.gen_range(0..=50) as u8;
+        let gray = rng.gen_range(113..=128) as u8;
         Self {
             // implement its sprite with:
             // 1. color
@@ -70,7 +68,7 @@ impl BedRockParticle {
             // 3. translation
             sprite: SpriteBundle {
                 sprite: Sprite {
-                    color: Color::srgb_u8(black, black, black),
+                    color: Color::srgb_u8(gray, gray, gray),
                     custom_size: Some(Vec2::splat(PARTICLE_SIZE)),
                     ..default()
                 },
@@ -152,96 +150,4 @@ impl SandParticle {
     // fn new() -> Self {
         
     // }
-}
-
-#[derive(Component)]
-pub struct ParticleElementDirt;
-
-#[derive(Bundle)]
-pub struct DirtParticle {
-    sprite: SpriteBundle, 
-    particle: Particle, 
-    element: ParticleElementDirt,
-}
-
-impl DirtParticle {
-    pub fn new(x: i32, y: i32) -> Self {
-        let mut rng = rand::thread_rng();
-        let r = rng.gen_range(118..=133) as u8;
-        let g = rng.gen_range(85..=90) as u8;
-        let b = rng.gen_range(43..=73) as u8;
-        Self {
-            sprite: SpriteBundle {
-                sprite: Sprite {
-                    color: Color::srgb_u8(r, g, b),
-                    custom_size: Some(Vec2::splat(PARTICLE_SIZE)),
-                    ..default()
-                },
-                transform: Transform {
-                    translation: Vec3::new(
-                        x as f32 * PARTICLE_SIZE + PARTICLE_SIZE / 2.,
-                        y as f32 * PARTICLE_SIZE + PARTICLE_SIZE / 2.,
-                        0.0,
-                    ),
-                    ..default()
-                },
-                ..default()
-            },
-            particle: Particle {
-                position: ParticlePosition::new(x, y),
-                data: ParticleData::Dirt,
-            },
-            element: ParticleElementDirt,
-        }
-    }
-}
-
-// the component for queries
-#[derive(Component)]
-pub struct ParticleElementStone;
-
-// the bundle that makes it a particle
-#[derive(Bundle)]
-pub struct StoneParticle {
-    sprite: SpriteBundle,
-    particle: Particle,
-    element: ParticleElementStone,
-}
-
-// if you are adding new particles (mainly procedural) follow this implementation scheme:
-impl StoneParticle {
-    pub fn new(x: i32, y: i32) -> Self {
-        // random color range
-        let mut rng = rand::thread_rng();
-        let gray = rng.gen_range(113..=128) as u8;
-        Self {
-            // implement its sprite with:
-            // 1. color
-            // 2. size
-            // 3. translation
-            sprite: SpriteBundle {
-                sprite: Sprite {
-                    color: Color::srgb_u8(gray, gray, gray),
-                    custom_size: Some(Vec2::splat(PARTICLE_SIZE)),
-                    ..default()
-                },
-                transform: Transform {
-                    translation: Vec3::new(
-                        x as f32 * PARTICLE_SIZE + PARTICLE_SIZE / 2.,
-                        y as f32 * PARTICLE_SIZE + PARTICLE_SIZE / 2.,
-                        0.0,
-                    ),
-                    ..default()
-                },
-                ..default()
-            },
-            // give the particle its position and data
-            particle: Particle {
-                position: ParticlePosition::new(x, y),
-                data: ParticleData::Stone,
-            },
-            // name the particle for queries
-            element: ParticleElementStone,
-        }
-    }
 }
