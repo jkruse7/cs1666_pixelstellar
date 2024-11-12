0016bdb44104df2b7853949c37300e7cc1b8530c
Jordan Shopp <jordan.shopp123@gmail.com>
2024-11-12 14:45:12 -0500

    Fixed concurrency problems

--- src/entities/player/systems.rs
+++ src/entities/player/systems.rs
@@ -236,44 +236,14 @@ fn water_splash(
             let center_x = (top_left_x + bottom_right_x) / 2;
             let mut row_offset = 0;
             let mut count = 0;
-            // info!("Number of water particles to splash: {}", num_water_particles_to_splash);
 
-            // let mut num_while_ran = 0;
-            // while (num_while_ran == 0) {
-            //     info!("Coords: {}, {}, {}, {}", top_left_x, top_left_y, bottom_right_x, bottom_right_y);
-            //     let lower_x = min(bottom_right_x, top_left_x);
-            //     let upper_x = max(bottom_right_x, top_left_x);
-            //     let lower_y = min(bottom_right_y, top_left_y);
-            //     let upper_y = max(bottom_right_y, top_left_y);
-            //     for i in lower_x..upper_x{
-            //         for j in lower_y..upper_y{
-            //             if map.get_element_at((i, j)) == ParticleElement::Water {
-            //                 info!("Giving velocity");
-            //                 map.delete_at(&mut commands, (i,j));
-            //                 //map.give_velocity(&mut commands, (i, j), Vec2::new(0., 10000.)); 
-            //                 //map.insert_at(commands, pos, list)
-            //                 map.insert_at::<WaterParticle>(&mut commands, (i, j+5), ListType::OnlyAir);
-                            
-
-            //                 count += 1;
-            //                 if count >= num_water_particles_to_splash {
-            //                     break;
-            //                 }
-            //             }
-            //         }
-            //     }
-            //     num_while_ran += 1;
-            // }
-            
-
+            let y_splash_distance = get_y_splash_distance(pv.velocity.y.abs());
 
             loop {
                 let y = top_left_y - row_offset;
 
                 // Stop at bottom row
                 if y < bottom_right_y || count >= num_water_particles_to_splash {
-                    // info!("Count: {}", count);
-                    // info!("Breaking");
                     break;
                 }
 
@@ -281,23 +251,19 @@ fn water_splash(
                 for offset in 0..=(bottom_right_x - top_left_x) / 2 {
                     let left_x = center_x.saturating_sub(offset);
                     let right_x = center_x + offset;
-                    if left_x >= top_left_x {
-                        if map.get_element_at((left_x, y)) == ParticleElement::Water {
-                            //give velocity
-                            //map.give_velocity(&mut commands, (left_x, y), Vec2::new(0., 100.)); 
-                            map.delete_at(&mut commands, (left_x, y));
-                            map.insert_at::<WaterParticle>(&mut commands, (left_x, y+7), ListType::OnlyAir);
-                            count += 1;
-                            if count >= num_water_particles_to_splash {
-                                break;
-                            }
+
+                    if left_x >= top_left_x && map.get_element_at((left_x, y)) == ParticleElement::Water{
+                        map.delete_at(&mut commands, (left_x, y));
+                        map.insert_at::<WaterParticle>(&mut commands, (left_x, y+y_splash_distance), ListType::OnlyAir);
+                        count += 1;
+                        if count >= num_water_particles_to_splash {
+                            break;
                         }
                     }
 
-                    if right_x <= bottom_right_x && map.get_element_at((right_x, y)) == ParticleElement::Water {
-                        //map.give_velocity(&mut commands, (right_x, y), Vec2::new(0., 100.)); 
+                    if right_x <= bottom_right_x && map.get_element_at((right_x, y+y_splash_distance)) == ParticleElement::Water {
                         map.delete_at(&mut commands, (right_x,y));
-                        map.insert_at::<WaterParticle>(&mut commands, (right_x, y+7), ListType::OnlyAir);
+                        map.insert_at::<WaterParticle>(&mut commands, (right_x, y+y_splash_distance), ListType::OnlyAir);
                         count += 1;
                         if count >= num_water_particles_to_splash {
                             break;
@@ -311,6 +277,26 @@ fn water_splash(
     }
 
     new_ratio
+}
+
+fn get_y_splash_distance(y_velocity: f32) -> i32 {
+    if y_velocity < 0. {
+        0
+    } else if y_velocity < 3. {
+        2
+    } else if y_velocity < 5. {
+        4
+    } else if y_velocity < 12. {
+        6
+    } else if  y_velocity < 30. {
+        8
+    } else if  y_velocity < 70. {
+        10
+    } else if y_velocity < 150. {
+        12
+    } else {
+        14
+    }
 }
 
 pub struct PlayerPlugin;
@@ -333,7 +319,7 @@ impl Plugin for PlayerPlugin {
         
         // Update events
         app.add_systems(Update, super::blaster::systems::update_blaster_aim);
-        app.add_systems(Update, super::blaster::systems::shoot_blaster);
+        app.add_systems(Update, super::blaster::systems::shoot_blaster.after(super::systems::flight));
         app.add_systems(Update, super::blaster::systems::handle_blaster_change_input);
         app.add_systems(Update, super::blaster::systems::change_blaster_on_event);
      //   app.add_system(super::blaster::systems::switch_blaster.system());
