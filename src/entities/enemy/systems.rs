8579fa9d45dc8e61c3647524702485b5f6d51eaa
jahnga <edm47@pitt.edu>
2024-12-08 21:06:46 -0500

    finish planet 4

--- src/entities/enemy/systems.rs
+++ src/entities/enemy/systems.rs
@@ -97,6 +97,40 @@ pub fn initialize(
             Enemy,
         ));
     } else if *game_state == GamePhase::Planet4 {
+         //let enemy_sheet_handle = asset_server.load("enemy_walking.png");
+         let enemy_sheet_handle = asset_server.load("planet_4/enemy_walking.png");
+         //             used to be tilesize. removed TILE_SIZE and now at 100, but change as needed  \/
+         let enemy_layout = TextureAtlasLayout::from_grid(UVec2::new(W4_SPRITE_WIDTH, W4_SPRITE_HEIGHT), 4, 1, None, None);
+         let enemy_layout_len = enemy_layout.textures.len();
+         let enemy_layout_handle = texture_atlases.add(enemy_layout);
+         commands.spawn((
+             SpriteBundle {
+                 texture: enemy_sheet_handle,
+                 transform: Transform {
+                     // Julianne 10/8: For now, enemy is being spawned at WIN_W. This will need to be changed eventually.
+                     translation: Vec3::new(WIN_W / 2., 100.0, 900.),
+                     ..default()
+                 },
+                 sprite: Sprite {
+                     flip_x: false,
+                     ..default()
+                 },
+                 ..default()
+             },
+             TextureAtlas {
+                 layout: enemy_layout_handle,
+                 index: 0,
+             },
+             AnimationTimer(Timer::from_seconds(W4_ANIM_TIME, TimerMode::Repeating)),
+             AnimationFrameCount(enemy_layout_len),
+             Velocity::new(),
+             EnemyHealth::new(5.),
+             Gravity::new(),
+             Hitbox::new(40 as f32, 40 as f32, Vec2::new(0., -210.)),
+             DamageBox::new(50.0, 50.0, Vec2::new(0., -210.)),
+             Jump::new(),  
+             Enemy,
+         ));
 
     } else if *game_state == GamePhase::Planet5 {
          //let enemy_sheet_handle = asset_server.load("enemy_walking.png");
@@ -250,6 +284,48 @@ pub fn enemy_gravity(
     } else if *game_state == GamePhase::Planet3 {
 
     } else if *game_state == GamePhase::Planet4 {
+        /*Julianne 10/8: This function is the same as player flight, but only makes the downward force on the enemy (no flight)*/
+        for (mut pt, mut pv, mut pg, mut hb, mut e_jump) in &mut enemy{
+
+            let deltat = time.delta_seconds();
+
+            //update gravity here
+            if e_jump.needs_jump && !e_jump.jumped{
+                pg.reset_g();
+                let acc_y = W4_ACCEL_RATE_Y * deltat;
+                pv.velocity.y = f32::min(250., pv.velocity.y + (1. * acc_y));
+                e_jump.needs_jump = false;
+                e_jump.is_jumping = true;
+            }else {
+                pg.update_g(&pv.velocity.y, &deltat, &grav_res);
+                pv.velocity.y = pg.get_g();
+            }
+            
+
+            let change = pv.velocity * deltat;
+            let new_pos = pt.translation + change.extend(0.);
+            let new_hb = Hitbox::new(W4_SPRITE_WIDTH as f32, W4_SPRITE_HEIGHT as f32, new_pos.xy());
+            //Bound enemy to within level height
+            if new_pos.y >= -(LEVEL_H / 2.) + (W4_SPRITE_HEIGHT as f32) / 2.
+                && new_pos.y <= LEVEL_H - (W4_SPRITE_HEIGHT as f32) / 2.
+                && (!new_hb.all_enemy_collisions(&hitboxes)) && !e_jump.jumped
+            {    
+
+                    pt.translation = new_pos;
+                    *hb = new_hb; 
+                    e_jump.jumped = true;
+            }  
+            let new_hb = Hitbox::new(W4_SPRITE_WIDTH as f32, W4_SPRITE_HEIGHT as f32,Vec2::new(new_pos.x + 1., new_pos.y));
+            // Velocity is zero when enemy hits the ground
+            if pt.translation.y <= -(LEVEL_H / 2.) + (W4_SPRITE_HEIGHT as f32) ||
+                new_hb.all_enemy_collisions(&hitboxes)
+            {
+                pv.velocity.y = 0.;
+                e_jump.is_jumping = false;
+                e_jump.jumped = false;
+                
+            }
+        }
 
     } else if *game_state == GamePhase::Planet5 {
         /*Julianne 10/8: This function is the same as player flight, but only makes the downward force on the enemy (no flight)*/
@@ -552,10 +628,87 @@ pub fn track_player(
             }
         }
 
-    }
-    else if *game_state == GamePhase::Planet4 {
-    }
-    else if *game_state == GamePhase::Planet5 {
+    } else if *game_state == GamePhase::Planet4 {
+        for (mut et, mut ev, mut es, mut ehb, mut timer, mut e_jump) in &mut enemy{
+            let (pt, mut player_health) = player.single_mut();
+            let player_hb = player_hitbox.single_mut();
+            
+            let cam_t = camera.single_mut();
+            let mut deltav_x = 0.;
+        
+            // if the player is hiding in quicksand, the enemy will not track player
+            let ratio_of_quicksand_particles = player_hb.ratio_of_quicksand_grid_tiles(&map);
+            //player needs to be more than half submerged to hide
+            if ratio_of_quicksand_particles > 0.5 {
+                return;
+            }
+            
+            // Is enemy within the camera frame?
+            if et.translation.x > cam_t.translation.x + (WIN_W/2.){
+                return}
+            else{
+                timer.tick(time.delta());
+            }
+            //face player and walk towards player
+            if pt.translation.x >= et.translation.x {
+                deltav_x += 1.;
+                es.flip_x=false;
+            }
+            else{
+                deltav_x -= 1.;
+                es.flip_x = true;
+            }
+        
+            let deltat = time.delta_seconds();
+            let acc_x = W4_ACCEL_RATE_X * deltat;
+        
+            if deltav_x != 0. {
+                if ev.velocity.y >= 0. {
+                    ev.velocity.x = (ev.velocity.x + deltav_x * acc_x).clamp(-W4_ENEMY_SPEED, W4_ENEMY_SPEED);
+                }
+                else {
+                    ev.velocity.x = (ev.velocity.x + deltav_x * acc_x).clamp(-W4_ENEMY_SPEED * 0.3, W4_ENEMY_SPEED * 0.3);
+                }
+            } else if ev.velocity.x.abs() > acc_x {
+                ev.velocity.x -= ev.velocity.x.signum() * acc_x;
+            } else {
+                ev.velocity.x = 0.;
+            }
+        
+            let change = ev.velocity * deltat;
+            let new_pos = et.translation + change.extend(0.);
+            let new_hb = Hitbox::new(W4_SPRITE_WIDTH as f32, W4_SPRITE_HEIGHT as f32, new_pos.xy());
+            
+        
+            let mut no_jump = false;
+            if player_hb.collides_with(&new_hb) {
+                no_jump = true;
+                take_damage(&mut player_health, 1.0, &mut death_event, &asset_server, &mut commands, &mut sound_tracker, &time);
+        
+                //info!("Player hit! Current health: {:?}", player_health.current); // 记录伤害
+                if player_health.current == 0.{
+                    death_event.send(Death);
+                }
+            }
+            if new_pos.x >= -(WIN_W / 2.) + (W4_SPRITE_WIDTH as f32) / 2.
+                && new_pos.x <= LEVEL_W - (WIN_W / 2. + (W4_SPRITE_WIDTH as f32) / 2.)
+                && new_hb.all_enemy_collisions(&hitboxes) && !e_jump.is_jumping && !no_jump
+            {
+                ev.velocity.x = 0.;
+                e_jump.needs_jump = true;
+            }
+            if new_pos.x >= -(WIN_W / 2.) + (W4_SPRITE_WIDTH as f32) / 2.
+                && new_pos.x <= LEVEL_W - (WIN_W / 2. + (W4_SPRITE_WIDTH as f32) / 2.)
+                && !new_hb.all_enemy_collisions(&hitboxes)
+            {
+                et.translation = new_pos;
+                *ehb = new_hb;
+            }
+            
+        }
+        
+
+    } else if *game_state == GamePhase::Planet5 {
          //get enemy, player and camera
          for (mut et, mut ev, mut es, mut ehb, mut timer, mut e_jump) in &mut enemy{
             let (pt, mut player_health) = player.single_mut();
@@ -673,6 +826,12 @@ pub fn check_enemy_damage(
         }
     }
     else if *game_state == GamePhase::Planet4 {
+        for (entity, ehb, mut eHealth) in query.iter_mut() {
+            if ehb.are_any_grid_tiles_water(&map) {
+                // info!("Enemy hit by water particle");
+                eHealth.hp -= 1.;
+            }
+        }
 
     }
     else if *game_state == GamePhase::Planet5 {
